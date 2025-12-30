e761cdad36ca5a0a81c91a39240abb2b1ebe787d
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-31 00:00:04 +0100

    add sand falling to the sides to simulation

--- src/simulate.rs
+++ src/simulate.rs
@@ -5,9 +5,22 @@ pub fn simulate_particles(particle_grid: &mut [u8], height: u32, width: u32) -> 
     for y in (0..height - 1).rev() {
         for x in 0..width {
             let idx: usize = (y * width + x) as usize;
-            if particle_grid[idx] == 1 && particle_grid[idx + width] == 0 {
-                particle_grid[idx] = 0;
-                particle_grid[idx + width] = 1;
+            // If there's a particle here and the space below is empty, move it down
+            if particle_grid[idx] == 1 {
+                if (particle_grid[idx + width /* last row is never checked */] == 0) {
+                    particle_grid[idx] = 0;
+                    particle_grid[idx + width] = 1;
+                }
+                // Try to move down-right
+                else if x < width - 1 && particle_grid[idx + width + 1] == 0 {
+                    particle_grid[idx] = 0;
+                    particle_grid[idx + width + 1] = 1;
+                }
+                // Try to move down-left
+                else if x > 0 && particle_grid[idx + width - 1] == 0 {
+                    particle_grid[idx] = 0;
+                    particle_grid[idx + width - 1] = 1;
+                }
             }
         }
     }
