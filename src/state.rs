c58533631056259c7621f91278bef6583ae9cb36
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-30 12:10:47 +0100

    add wgpu attributes to State and implement State::new()

--- src/state.rs
+++ src/state.rs
@@ -3,12 +3,79 @@ use std::sync::Arc;
 use winit::window::Window;
 
 pub struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
     pub(crate) window: Arc<Window>,
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+        // The instance is a handle to our GPU -> its main purpose is to create the surface and adapter
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        // The surface is the part of the window to draw to.
+        let surface = instance.create_surface(window.clone())?;
+
+        // The adapter is a handle for our actual graphics card.
+        // You can use this to get information about the graphics card.
+        // WARN: The options passed to request_adapter aren't guaranteed to work for all devices.
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                // The force_fallback_adapter forces wgpu to pick an adapter that will work on all hardware.
+                // This usually means that the rendering backend will use a "software" system instead of hardware such as a GPU.
+                force_fallback_adapter: false,
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                experimental_features: wgpu::ExperimentalFeatures::disabled(),
+                required_limits: wgpu::Limits::default(),
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: window.inner_size().width,
+            height: window.inner_size().height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+            window,
+        })
     }
 
     pub fn resize(&mut self, _width: u32, _height: u32) {
