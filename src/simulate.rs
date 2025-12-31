66abef518c5cad719987e3dea0425cfebd524251
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-31 10:46:46 +0100

    add comment to clarify simulate.rs

--- src/simulate.rs
+++ src/simulate.rs
@@ -2,6 +2,7 @@ pub fn simulate_particles(particle_grid: &mut [u8], height: u32, width: u32) -> 
     let height = height as usize;
     let width = width as usize;
 
+    // Goes from the bottom to the top and simulates each particle
     for y in (0..height - 1).rev() {
         for x in 0..width {
             let idx: usize = (y * width + x) as usize;
